9b71dc38cda8b689d1758a330efcf4f082f14551
cheezi0747 <linus.sjolinder@b3.se>
2024-03-19 08:51:00 +0100

    test: removed javers annotation from messageattachment repository

--- src/main/java/se/sundsvall/casedata/integration/db/MessageAttachmentRepository.java
+++ src/main/java/se/sundsvall/casedata/integration/db/MessageAttachmentRepository.java
@@ -1,13 +1,11 @@
 package se.sundsvall.casedata.integration.db;
 
-import org.javers.spring.annotation.JaversSpringDataAuditable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import se.sundsvall.casedata.integration.db.model.MessageAttachment;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
-@JaversSpringDataAuditable
 @CircuitBreaker(name = "messageAttachmentRepository")
 public interface MessageAttachmentRepository extends JpaRepository<MessageAttachment, String> {
 
