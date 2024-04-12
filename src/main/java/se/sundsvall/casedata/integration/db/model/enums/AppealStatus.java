ddf01b1aa1b9ffd7339b9f6f2e2d7fd505b565c8
Robin Rönnberg <robin.ronnberg@sundsvall.se>
2024-04-12 09:26:57 +0200

    UF-7816: Add Appeal DB entity

---
+++ src/main/java/se/sundsvall/casedata/integration/db/model/enums/AppealStatus.java
@@ -0,0 +1,11 @@
+package se.sundsvall.casedata.integration.db.model.enums;
+
+/**
+ * NEW (Ny)
+ * REJECTED (Avslag)
+ * SENT_FOR_TRIAL (Skickad till rättsinstans)
+ * COMPLETED (Klar)
+ */
+public enum AppealStatus {
+	NEW, REJECTED, SENT_FOR_TRIAL, COMPLETED
+}
---
+++ src/main/java/se/sundsvall/casedata/integration/db/model/enums/TrailStatus.java
@@ -0,0 +1,11 @@
+package se.sundsvall.casedata.integration.db.model.enums;
+
+/**
+ * NOT_CONDUCTED (Ej prövad)
+ * NOT_RELEVANT (Ej relevant)
+ * APPROVED (Godkänd)
+ * REJECTED  (Avslag)
+ */
+public enum TrailStatus {
+	NOT_CONDUCTED, NOT_RELEVANT, APPROVED, REJECTED
+}
