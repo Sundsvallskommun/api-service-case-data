acafa317740d1fb76ed446f9f6023921bb4a4c95
Robin Rönnberg <robin.ronnberg@sundsvall.se>
2024-04-12 11:55:42 +0200

    UF-7816: Fix review comments

--- src/main/java/se/sundsvall/casedata/integration/db/model/enums/TimelinessReview.java
+++ src/main/java/se/sundsvall/casedata/integration/db/model/enums/TimelinessReview.java
@@ -1,11 +1,13 @@
 package se.sundsvall.casedata.integration.db.model.enums;
 
 /**
+ * TimelinessReview (Rättidsprövning):
+ *
  * NOT_CONDUCTED (Ej prövad)
  * NOT_RELEVANT (Ej relevant)
  * APPROVED (Godkänd)
  * REJECTED  (Avslag)
  */
-public enum TrailStatus {
+public enum TimelinessReview {
 	NOT_CONDUCTED, NOT_RELEVANT, APPROVED, REJECTED
 }
