a423162c8558e62e679bc88132eb4dbaed254b83
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2024-04-26 16:04:30 +0200

    UF-8023:

--- src/test/resources/db/scripts/truncate.sql
+++ src/test/resources/db/scripts/truncate.sql
@@ -11,6 +11,7 @@ TRUNCATE table errand_statuses;
 --TRUNCATE table errand_message_ids;
 
 TRUNCATE table facility;
+TRUNCATE table facility_extra_parameters;
 
 TRUNCATE table note;
 TRUNCATE table note_extra_parameters;
