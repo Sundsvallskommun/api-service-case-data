219123f1495b59ae53582ba2d0b3e4a1dd922dc4
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2024-04-26 16:02:41 +0200

    UF-8023: Fix mistakes in SQL-scripts

--- src/test/resources/db/scripts/truncate.sql
+++ src/test/resources/db/scripts/truncate.sql
@@ -10,7 +10,7 @@ TRUNCATE table errand_extra_parameters;
 TRUNCATE table errand_statuses;
 --TRUNCATE table errand_message_ids;
 
---TRUNCATE table facility;
+TRUNCATE table facility;
 
 TRUNCATE table note;
 TRUNCATE table note_extra_parameters;
