d4c4068f61d35727516ffa1c157d6202f358e9dc
cheezi0747 <linus.sjolinder@b3.se>
2024-10-09 09:41:56 +0200

    UF-10397: Fixes after review

--- src/main/java/se/sundsvall/casedata/api/model/Suspension.java
+++ src/main/java/se/sundsvall/casedata/api/model/Suspension.java
@@ -1,7 +1,6 @@
 package se.sundsvall.casedata.api.model;
 
 import java.time.OffsetDateTime;
-import java.util.Objects;
 
 import jakarta.validation.constraints.FutureOrPresent;
 
@@ -12,9 +11,11 @@ import se.sundsvall.casedata.api.model.validation.ValidSuspension;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Setter
 @Getter
@@ -22,6 +23,8 @@ import lombok.Setter;
 @AllArgsConstructor
 @Builder(setterPrefix = "with")
 @ValidSuspension
+@EqualsAndHashCode
+@ToString
 public class Suspension {
 
 	@Schema(description = "Timestamp when the suspension wears off", example = "2000-10-31T01:30:00.000+02:00")
@@ -32,26 +35,5 @@ public class Suspension {
 	@Schema(description = "Timestamp when the suspension started", example = "2000-10-31T01:30:00.000+02:00")
 	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
 	private OffsetDateTime suspendedFrom;
-
-	@Override
-	public String toString() {
-		return "Suspension{" +
-			"suspendedTo=" + suspendedTo +
-			", suspendedFrom=" + suspendedFrom +
-			'}';
-	}
-
-	@Override
-	public boolean equals(final Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		final Suspension that = (Suspension) o;
-		return Objects.equals(suspendedTo, that.suspendedTo) && Objects.equals(suspendedFrom, that.suspendedFrom);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(suspendedTo, suspendedFrom);
-	}
 
 }
