197583486e6c87fd888d00e8606a765b6bae033d
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2024-10-16 10:01:08 +0200

    UF-10464: Removed deprecated MessageAttachment resource

--- src/test/java/se/sundsvall/casedata/api/MessageAttachmentResourceTest.java
+++ src/test/java/se/sundsvall/casedata/api/MessageAttachmentResourceTest.java
@@ -1,13 +1,10 @@
 package se.sundsvall.casedata.api;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.mockito.Mockito.when;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static se.sundsvall.casedata.TestUtil.MUNICIPALITY_ID;
 import static se.sundsvall.casedata.TestUtil.NAMESPACE;
 
@@ -19,44 +16,19 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import se.sundsvall.casedata.Application;
-import se.sundsvall.casedata.api.model.MessageAttachment;
 import se.sundsvall.casedata.service.MessageService;
 
 @SpringBootTest(classes = Application.class, webEnvironment = RANDOM_PORT)
 @ActiveProfiles("junit")
 class MessageAttachmentResourceTest {
 
-	private static final String BASE_URL = "/{municipalityId}/{namespace}/errands/{errandId}/messageattachments";
+	private static final String PATH = "/{municipalityId}/{namespace}/errands/{errandId}/messageattachments/{attachmentId}/streamed";
 
 	@MockBean
 	private MessageService messageServiceMock;
 
 	@Autowired
 	private WebTestClient webTestClient;
-
-	@Test
-	void getMessageAttachment() {
-		// Arrange
-		final var errandId = 123L;
-		final var attachmentId = "attachment123";
-		final var messageAttachment = new MessageAttachment();
-		when(messageServiceMock.getMessageAttachment(errandId, attachmentId, MUNICIPALITY_ID, NAMESPACE)).thenReturn(messageAttachment);
-
-		// Act
-		var response = webTestClient.get()
-			.uri(uriBuilder -> uriBuilder.path(BASE_URL + "/{attachmentId}").build(MUNICIPALITY_ID, NAMESPACE, errandId, attachmentId))
-			.exchange()
-			.expectStatus().isOk()
-			.expectHeader().contentType(APPLICATION_JSON_VALUE)
-			.expectBody(MessageAttachment.class)
-			.returnResult()
-			.getResponseBody();
-
-		// Assert
-		assertThat(response).isNotNull();
-		verify(messageServiceMock).getMessageAttachment(errandId, attachmentId, MUNICIPALITY_ID, NAMESPACE);
-		verifyNoMoreInteractions(messageServiceMock);
-	}
 
 	@Test
 	void getMessageAttachmentStreamed() {
@@ -66,7 +38,7 @@ class MessageAttachmentResourceTest {
 
 		// Act
 		webTestClient.get()
-			.uri(uriBuilder -> uriBuilder.path(BASE_URL + "/{attachmentId}/streamed").build(MUNICIPALITY_ID, NAMESPACE, errandId, attachmentId))
+			.uri(uriBuilder -> uriBuilder.path(PATH).build(MUNICIPALITY_ID, NAMESPACE, errandId, attachmentId))
 			.exchange()
 			.expectStatus().isOk();
 
@@ -74,5 +46,4 @@ class MessageAttachmentResourceTest {
 		verify(messageServiceMock).getMessageAttachmentStreamed(eq(errandId), eq(attachmentId), eq(MUNICIPALITY_ID), eq(NAMESPACE), any());
 		verifyNoMoreInteractions(messageServiceMock);
 	}
-
 }
