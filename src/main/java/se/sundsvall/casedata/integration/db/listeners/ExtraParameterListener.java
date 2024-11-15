593ec05a1d45697bcb4be70e8217ee4b0a263105
cheezi0747 <linus.sjolinder@b3.se>
2024-11-15 11:23:04 +0100

    DRAKEN-686: Changes after review

--- src/main/java/se/sundsvall/casedata/integration/db/listeners/ExtraParameterListener.java
+++ src/main/java/se/sundsvall/casedata/integration/db/listeners/ExtraParameterListener.java
@@ -1,6 +1,6 @@
 package se.sundsvall.casedata.integration.db.listeners;
 
-import jakarta.persistence.PostPersist;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreRemove;
 import jakarta.persistence.PreUpdate;
 import org.springframework.stereotype.Component;
@@ -10,16 +10,13 @@ import se.sundsvall.casedata.integration.db.model.ExtraParameterEntity;
 @Component
 public class ExtraParameterListener {
 
-	private final IncomingRequestFilter incomingRequestFilter;
-
 	private final ErrandListener errandListener;
 
 	public ExtraParameterListener(final IncomingRequestFilter incomingRequestFilter, final ErrandListener errandListener) {
-		this.incomingRequestFilter = incomingRequestFilter;
 		this.errandListener = errandListener;
 	}
 
-	@PostPersist
+	@PrePersist
 	private void postPersist(final ExtraParameterEntity extraParameterEntity) {
 		errandListener.updateErrandFields(extraParameterEntity.getErrand());
 	}
