d4464a9847856d97c1689c6524d46d850f7297a8
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2024-12-17 11:20:03 +0100

    UF-11345: Add circuitbreaker-annotation

--- src/main/java/se/sundsvall/casedata/integration/parkingpermit/ParkingPermitClient.java
+++ src/main/java/se/sundsvall/casedata/integration/parkingpermit/ParkingPermitClient.java
@@ -3,21 +3,20 @@ package se.sundsvall.casedata.integration.parkingpermit;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static se.sundsvall.casedata.integration.parkingpermit.configuration.ParkingPermitConfiguration.CLIENT_ID;
 
+import generated.se.sundsvall.parkingpermit.StartProcessResponse;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import se.sundsvall.casedata.integration.parkingpermit.configuration.ParkingPermitConfiguration;
-
-import generated.se.sundsvall.parkingpermit.StartProcessResponse;
-import io.github.resilience4j.retry.annotation.Retry;
 
 @FeignClient(
 	name = CLIENT_ID,
 	url = "${integration.parkingpermit.base-url}",
-	configuration = ParkingPermitConfiguration.class
-)
+	configuration = ParkingPermitConfiguration.class)
+@CircuitBreaker(name = CLIENT_ID)
 public interface ParkingPermitClient {
 
 	@Retry(name = CLIENT_ID)
@@ -31,5 +30,4 @@ public interface ParkingPermitClient {
 	ResponseEntity<Void> updateProcess(
 		@PathVariable(name = "municipalityId") final String municipalityId,
 		@PathVariable(name = "processInstanceId") final String processInstanceId);
-
 }
