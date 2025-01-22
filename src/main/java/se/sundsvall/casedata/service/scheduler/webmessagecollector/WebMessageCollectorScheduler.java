51df5402f01ad3bf772e61d53ef927d26eb6bc49
cheezi0747 <linus.sjolinder@b3.se>
2025-01-22 10:55:33 +0100

    UF-11757: Use dept44-scheduler

--- src/main/java/se/sundsvall/casedata/service/scheduler/webmessagecollector/WebMessageCollectorScheduler.java
+++ src/main/java/se/sundsvall/casedata/service/scheduler/webmessagecollector/WebMessageCollectorScheduler.java
@@ -1,12 +1,10 @@
 package se.sundsvall.casedata.service.scheduler.webmessagecollector;
 
-import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import se.sundsvall.dept44.requestid.RequestId;
+import se.sundsvall.dept44.scheduling.Dept44Scheduled;
 
 @Service
 @ConditionalOnProperty(prefix = "scheduler.message-collector", name = "enabled", havingValue = "true", matchIfMissing = true)
@@ -20,21 +18,20 @@ public class WebMessageCollectorScheduler {
 		this.webMessageCollectorWorker = webMessageCollectorWorker;
 	}
 
-	@Scheduled(initialDelayString = "${scheduler.message-collector.initialDelay}", fixedRateString = "${scheduler.message-collector.fixedRate}", zone = "Europe/Stockholm")
-	@SchedulerLock(name = "message-collector", lockAtMostFor = "${scheduler.message-collector.shedlock-lock-at-most-for}")
+	@Dept44Scheduled(
+		cron = "${scheduler.message-collector.cron}",
+		name = "${scheduler.message-collector.name}",
+		lockAtMostFor = "${scheduler.message-collector.shedlock-lock-at-most-for}",
+		maximumExecutionTime = "${scheduler.message-collector.maximum-execution-time}")
 	public void getAndProcessMessages() {
-		try {
-			RequestId.init();
 
-			LOG.info("Getting and processing messages");
-			final var messageMap = webMessageCollectorWorker.getAndProcessMessages();
-			LOG.info("Finished getting and processing messages");
+		LOG.info("Getting and processing messages");
+		final var messageMap = webMessageCollectorWorker.getAndProcessMessages();
+		LOG.info("Finished getting and processing messages");
 
-			LOG.info("Deleting messages from WebMessageCollector");
-			webMessageCollectorWorker.deleteMessages(messageMap);
-			LOG.info("Finished deleting messages from WebMessageCollector");
-		} finally {
-			RequestId.reset();
-		}
+		LOG.info("Deleting messages from WebMessageCollector");
+		webMessageCollectorWorker.deleteMessages(messageMap);
+		LOG.info("Finished deleting messages from WebMessageCollector");
+
 	}
 }
