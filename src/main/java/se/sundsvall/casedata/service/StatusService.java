83ed911aaa19b4485af6241d48f63030ca4b030e
cheezi0747 <linus.sjolinder@b3.se>
2025-02-06 11:46:13 +0100

    UF-14322: Set status when updating statuses

--- src/main/java/se/sundsvall/casedata/service/StatusService.java
+++ src/main/java/se/sundsvall/casedata/service/StatusService.java
@@ -29,6 +29,7 @@ public class StatusService {
 	public void addToErrand(final Long errandId, final String municipalityId, final String namespace, final Status status) {
 		final var oldErrand = findErrandEntity(errandId, municipalityId, namespace);
 		final var statusEntity = toStatusEntity(status);
+		oldErrand.setStatus(status.getStatusType());
 		oldErrand.getStatuses().add(statusEntity);
 		final var updatedErrand = errandRepository.save(oldErrand);
 		processService.updateProcess(updatedErrand);
@@ -37,6 +38,8 @@ public class StatusService {
 	@Retry(name = "OptimisticLocking")
 	public void replaceOnErrand(final Long errandId, final String municipalityId, final String namespace, final List<Status> statuses) {
 		final var oldErrand = findErrandEntity(errandId, municipalityId, namespace);
+		oldErrand.setStatus(statuses.getFirst().getStatusType());
+
 		oldErrand.setStatuses(new ArrayList<>(statuses.stream().map(EntityMapper::toStatusEntity).toList()));
 		final var updatedErrand = errandRepository.save(oldErrand);
 		processService.updateProcess(updatedErrand);
