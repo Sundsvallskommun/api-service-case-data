23026e2ce82c91673750c5d614e436342de3ecf1
cheezi0747 <linus.sjolinder@b3.se>
2025-02-21 10:48:08 +0100

    UF-14590: Backport DB-script

--- src/main/resources/db/migration/V3_8__add_status_field_on_errand.sql
+++ src/main/resources/db/migration/V3_8__add_status_field_on_errand.sql
@@ -1,2 +1,8 @@
 alter table if exists errand
-    add column status varchar(255);
+    add column if not exists status varchar(255);
+
+alter table if exists errand
+    add column if not exists status_created datetime(6);
+
+alter table if exists errand
+    add column if not exists status_description varchar(255);
