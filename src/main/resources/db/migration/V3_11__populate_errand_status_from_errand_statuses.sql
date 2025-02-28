89d016299373601bd78ef3b4a3e9b3d4090fbea3
cheezi0747 <linus.sjolinder@b3.se>
2025-02-28 09:54:03 +0100

    UF-14321: Changes after review feedback

--- src/main/resources/db/migration/V3_11__populate_errand_status_from_errand_statuses.sql
+++ src/main/resources/db/migration/V3_11__populate_errand_status_from_errand_statuses.sql
@@ -1,13 +1,13 @@
 UPDATE errand e
-    JOIN (SELECT es1.errand_id, es1.status_type, es1.description, es1.created
+    JOIN (SELECT es1.errand_id, es1.status_type, es1.description, es1.date_time
           FROM errand_statuses es1
           WHERE NOT EXISTS (SELECT 1
                             FROM errand_statuses es2
                             WHERE es2.errand_id = es1.errand_id
-                              AND es2.created > es1.created)) ls ON e.id = ls.errand_id
+                              AND es2.date_time > es1.date_time)) ls ON e.id = ls.errand_id
 SET e.status             = ls.status_type,
     e.status_description = ls.description,
-    e.status_created     = ls.created
+    e.status_created     = ls.date_time
 where e.description is null
    or e.status is null
    or e.status_created is null;
