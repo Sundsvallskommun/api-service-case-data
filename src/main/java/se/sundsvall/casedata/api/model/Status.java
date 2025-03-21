31f35b918f1e17981ad32d73f5af9a8656244541
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2025-03-21 13:19:23 +0100

    UF-15141: Fix API issue

--- src/main/java/se/sundsvall/casedata/api/model/Status.java
+++ src/main/java/se/sundsvall/casedata/api/model/Status.java
@@ -1,5 +1,8 @@
 package se.sundsvall.casedata.api.model;
 
+import static io.swagger.v3.oas.annotations.media.Schema.AccessMode.READ_ONLY;
+import static io.swagger.v3.oas.annotations.media.Schema.AccessMode.READ_WRITE;
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 import static org.hibernate.annotations.TimeZoneStorageType.NORMALIZE;
 
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -16,11 +19,12 @@ import org.hibernate.annotations.TimeZoneStorage;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder(setterPrefix = "with")
+@Schema(description = "The status model", accessMode = READ_WRITE)
 public class Status {
 
 	@NotBlank
 	@Size(max = 255)
-	@Schema(description = "The type of status", example = "Ärende inkommit", maxLength = 255, requiredMode = Schema.RequiredMode.REQUIRED)
+	@Schema(description = "The type of status", example = "Ärende inkommit", maxLength = 255, requiredMode = REQUIRED)
 	private String statusType;
 
 	@Size(max = 255)
@@ -28,7 +32,6 @@ public class Status {
 	private String description;
 
 	@TimeZoneStorage(NORMALIZE)
-	@Schema(description = "The date and time when the status was created", example = "2023-01-01T12:00:00Z", accessMode = Schema.AccessMode.READ_ONLY)
+	@Schema(description = "The date and time when the status was created", example = "2023-01-01T12:00:00Z", accessMode = READ_ONLY)
 	private OffsetDateTime created;
-
 }
