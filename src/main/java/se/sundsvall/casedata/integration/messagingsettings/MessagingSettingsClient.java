30e6f3c187e61ed298a26b9dbd1b32890965f287
cheezi0747 <linus.sjolinder@b3.se>
2025-06-11 08:13:05 +0200

    UF-16520: Message-Settings integration

---
+++ src/main/java/se/sundsvall/casedata/integration/messagingsettings/MessagingSettingsClient.java
@@ -0,0 +1,23 @@
+package se.sundsvall.casedata.integration.messagingsettings;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static se.sundsvall.casedata.integration.messagingsettings.configuration.MessagingSettingsConfiguration.CLIENT_ID;
+
+import generated.se.sundsvall.messagingsettings.SenderInfoResponse;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import se.sundsvall.casedata.integration.messagingsettings.configuration.MessagingSettingsConfiguration;
+
+@FeignClient(name = CLIENT_ID, url = "${integration.messaging-settings.base-url}", configuration = MessagingSettingsConfiguration.class)
+@CircuitBreaker(name = CLIENT_ID)
+public interface MessagingSettingsClient {
+
+	@GetMapping(path = "/{municipalityId}/{departmentId}/sender-info", produces = APPLICATION_JSON_VALUE)
+	ResponseEntity<SenderInfoResponse> getSenderInfo(
+		@PathVariable(name = "municipalityId") final String municipalityId,
+		@PathVariable(name = "departmentId") final String departmentId);
+
+}
