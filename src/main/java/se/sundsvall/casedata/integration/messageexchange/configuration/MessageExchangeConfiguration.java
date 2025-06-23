cc55bac0434e50d8096a24c894319217a242fbfa
cheezi0747 <linus.sjolinder@b3.se>
2025-06-23 12:43:53 +0200

    UF-16606: set sentby header when sending to messageExchange

--- src/main/java/se/sundsvall/casedata/integration/messageexchange/configuration/MessageExchangeConfiguration.java
+++ src/main/java/se/sundsvall/casedata/integration/messageexchange/configuration/MessageExchangeConfiguration.java
@@ -1,5 +1,10 @@
 package se.sundsvall.casedata.integration.messageexchange.configuration;
 
+import static com.google.common.base.CaseFormat.LOWER_CAMEL;
+import static com.google.common.base.CaseFormat.UPPER_UNDERSCORE;
+import static java.util.Optional.ofNullable;
+import static org.apache.commons.lang3.ObjectUtils.allNotNull;
+
 import org.springframework.cloud.openfeign.FeignBuilderCustomizer;
 import org.springframework.cloud.openfeign.support.JsonFormWriter;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +13,7 @@ import org.springframework.security.oauth2.client.registration.ClientRegistratio
 import se.sundsvall.dept44.configuration.feign.FeignConfiguration;
 import se.sundsvall.dept44.configuration.feign.FeignMultiCustomizer;
 import se.sundsvall.dept44.configuration.feign.decoder.ProblemErrorDecoder;
+import se.sundsvall.dept44.support.Identifier;
 
 @Import(FeignConfiguration.class)
 public class MessageExchangeConfiguration {
@@ -24,9 +30,17 @@ public class MessageExchangeConfiguration {
 	FeignBuilderCustomizer feignBuilderCustomizer(final ClientRegistrationRepository clientRepository, final MessageExchangeProperties messageExchangeProperties) {
 		return FeignMultiCustomizer.create()
 			.withErrorDecoder(new ProblemErrorDecoder(CLIENT_ID))
+			.withRequestInterceptor(builder -> builder.header(Identifier.HEADER_NAME, createSentByHeaderValue(Identifier.get())))
 			.withRequestTimeoutsInSeconds(messageExchangeProperties.connectTimeout(), messageExchangeProperties.readTimeout())
 			.withRetryableOAuth2InterceptorForClientRegistration(clientRepository.findByRegistrationId(CLIENT_ID))
 			.composeCustomizersToOne();
+	}
+
+	String createSentByHeaderValue(final Identifier identifier) {
+		return ofNullable(identifier)
+			.filter(i -> allNotNull(i.getType(), i.getValue()))
+			.map(i -> "%s; type=%s".formatted(ofNullable(i.getValue()).orElse(""), ofNullable(i.getType()).map(t -> UPPER_UNDERSCORE.to(LOWER_CAMEL, i.getType().name())).orElse("")))
+			.orElse(null);
 	}
 
 }
