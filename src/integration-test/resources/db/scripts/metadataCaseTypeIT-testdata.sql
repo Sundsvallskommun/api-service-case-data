95412c2eb6791d90d539371666e11a2b82b6d42d
cheezi0747 <linus.sjolinder@b3.se>
2025-09-11 13:14:05 +0200

    UF-17196: IT test for casetype

---
+++ src/integration-test/resources/db/scripts/metadataCaseTypeIT-testdata.sql
@@ -0,0 +1,4 @@
+INSERT INTO case_type(type, display_name, namespace, municipality_id)
+VALUES ('type-1', 'displayname-1', 'MY_NAMESPACE', '2281'),
+       ('type-2', 'displayname-2', 'MY_NAMESPACE', '2281'),
+       ('type-3', 'displayname-3', 'MY_NAMESPACE', '2281');
